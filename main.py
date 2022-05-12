71f71e5e3bfeb517d07fcdeadd77607e55bdfbee
GonzaloBalderrama <103964019+GonzaloBalderrama@users.noreply.github.com>
2022-05-12 17:54:36 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from Facultad import Facultad
-from Carrera import Carrera
-from ManejadorFacultades import ManejadorFacultades
-
-if __name__ == '__main__':
-    listaFacultades = ManejadorFacultades()
-    listaFacultades.crearFacultad()
