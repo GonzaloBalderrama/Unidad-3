6228c2938c3c29d8446e026cb7b3978702acf6ac
GonzaloBalderrama <103964019+GonzaloBalderrama@users.noreply.github.com>
2022-06-02 16:46:54 -0300

    Add files via upload

---
+++ Ejercicio 5/interfaz.py
@@ -0,0 +1,13 @@
+from zope.interface import Interface
+from zope.interface import implementer
+
+class InterfacePrueba(Interface):
+
+    def insertarElemento(posicion):
+        pass
+
+    def agregarElemento():
+        pass
+
+    def mostrarElemento(posicion):
+        pass
