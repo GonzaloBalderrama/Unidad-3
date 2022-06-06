3e27ef2dcd6168be2adf2744fc93d0ed12635497
GonzaloBalderrama <103964019+GonzaloBalderrama@users.noreply.github.com>
2022-06-06 12:44:14 -0300

    Add files via upload

--- Ejercicio 4/main.py
+++ Ejercicio 4/main.py
@@ -1,2 +1,10 @@
-if __name__=="__main__":
-    pass
+from coleccioncalefactores import ColeccionCalefactores
+
+if __name__=='__main__':
+    dimension=int(input('Ingrese cantidad de calefactores a gas a almacenar:'))
+    dimensione=int(input('Ingrese cantidad de calefactores electricos a almacenar:'))
+    manejacalefactores=ColeccionCalefactores(dimension,dimensione)
+    manejacalefactores.LeerArchivos()
+    gas=manejacalefactores.actividad1()
+    ele=manejacalefactores.actividad2()
+    manejacalefactores.actividad3(gas,ele)
